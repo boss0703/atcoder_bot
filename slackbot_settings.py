65add02a16352a70e9789d3fa065ed1347c1099e
boss0703 <prinnykurtis002@gmail.com>
2021-02-08 13:46:27 +0900

    APIKEY Set2

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,5 +1,4 @@
 import os
-from boto.s3.connection import S3Connection
-API_TOKEN = S3Connection(os.environ['API_TOKEN'])
+API_TOKEN = os.environ['API_TOKEN']
 DEFAULT_REPLY = "でふぉるとの返信"
 PLUGINS = ['plugins']
